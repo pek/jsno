bb368fb67abbdfc8cfdc61c024e83b8418ab8fd4
Pekka Uronen <pekka@huikea.com>
2023-07-15 12:30:22 +0300

    loads/dumps

--- jsno/__init__.py
+++ jsno/__init__.py
@@ -1,2 +1,12 @@
+import json
+
 from jsno.jsonify import jsonify
 from jsno.unjsonify import unjsonify, UnjsonifyError
+
+
+def dumps(value, **kwargs):
+    return json.dumps(jsonify(value), **kwargs)
+
+
+def loads(value, as_type, **kwargs):
+    return json.loads(unjsonify[as_type](value), **kwargs)
