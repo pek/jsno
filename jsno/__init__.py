016a46aef54def96a434348866a314bd2b4c5a7b
Pekka Uronen <pekka@huikea.com>
2023-07-16 11:22:37 +0300

    docstring

--- jsno/__init__.py
+++ jsno/__init__.py
@@ -1,3 +1,37 @@
+"""
+Jsonify Python objects to JSON-compatible values, and unjsonify it back to
+Python objects.
+
+Jsonify maps any Python object to a structure consisting of dicts, lists,
+and primitive values that is ready to be converted to JSON.
+
+Unjsonify converts such JSON-structure to a given type. The type to convert
+to must be given as argument.
+
+The following types are currently supported:
+    - int, float, bool
+    - None (null in JSON)
+    - lists and tuples
+    - dicts
+    - types defined as dataclass
+    - enum types
+    - date and datetime
+
+When unjsonising, lists and dicts need to be specified with List[T] and
+Dict[K,V] constructs.
+
+Any type can be made compatible by registering custom functions for converting
+to/from a json structure. Example converters for date and datetime are provided.
+
+Note that the jsoniser unjsonisers are matched in reverse registration order,
+so if there is a separate jsoniser/unjsoniser defined for both a subclass and
+it's superclass, -the superclass must be registered first.
+
+A custom class can be made compatible also by providing jsonify method and
+unjsonify class method.
+
+"""
+
 import json
 
 from jsno.jsonify import jsonify
