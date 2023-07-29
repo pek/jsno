3dfc5deb8b75fc2d312f6bfae850d73266ecff24
Pekka Uronen <pekka@huikea.com>
2023-07-29 11:48:44 +0300

    combine measurements

--- performance/measure_jsonify.py
+++ performance/measure_jsonify.py
@@ -1,14 +1,15 @@
+import dataclasses
 import datetime
 import json
 import sys
 
 
-from jsno import jsonify
+from jsno import jsonify, dumps, unjsonify
 from performance.utils import measure_time
-from performance.dataclasses import example_box
+from performance.dataclasses import example_box, Box
 
 
-def measure_case(n, item):
+def measure_case(n, item, as_type):
     name = item['name']
 
     items = {
@@ -21,7 +22,25 @@ def measure_case(n, item):
     with measure_time() as dump_time:
         dump = json.dumps(jsonified)
 
+    bytecount = len(dump)
+
+    with measure_time() as loads_time:
+        loaded = json.loads(dump)
+
+    @dataclasses.dataclass
+    class Items:
+        items: list[as_type]
+
+    with measure_time() as unjsonify_time:
+         unjsonify[Items](loaded)
+
     # microseconds per item
+    loads_per_ms = (n / loads_time.total * 1000)
+    unjsonify_per_ms = (n / unjsonify_time.total * 1000)
+
+    loads_bytes_per_ms = (bytecount / loads_time.total)
+    unjsonify_bytes_per_ms = (bytecount / unjsonify_time.total)
+
     jsonify_per_ms = (n / jsonify_time.total * 1000)
     dump_per_ms = (n / dump_time.total * 1000)
 
@@ -29,50 +48,87 @@ def measure_case(n, item):
     dump_bytes_per_ms = (len(dump) / dump_time.total)
 
     print(
-        f'{name:<28} ({len(dump):>8} bytes)'
-        f' | jsonify {jsonify_per_ms:>6.1f} items/ms'
+        f'{name:<28} ({bytecount:>8} bytes)'
+        f' | jsonify {jsonify_per_ms:>6.1f} n/ms'
         f' {jsonify_bytes_per_ms:>5.1f} kb/ms'
-        f' | dump {dump_per_ms:>6.1f} items/ms'
+        f' | dump {dump_per_ms:>6.1f} n/ms'
         f' {dump_bytes_per_ms:>5.1f} kb/ms'
+        f' | loads {loads_per_ms:>6.1f} n/ms'
+        f' {unjsonify_per_ms:>6.1f} kb/ms'
+        f' | unjsonify {loads_bytes_per_ms:>6.1f} n/ms'
+        f' {unjsonify_bytes_per_ms:>6.1f} kb/ms'
     )
 
 
 def main(n=10000):
     n = int(n)
-
     for _ in range(3):
         print()
         print(f'N = {n}')
 
-        measure_case(n, {
-            "name": "Pure JSON",
-            "counter": 2014123,
-            "enabled": False,
-            "number": 132.1
-        })
+        measure_case(
+            n,
+            {
+                "name": "Pure JSON",
+                "counter": 2014123,
+                "enabled": False,
+                "number": 132.1
+            },
+            as_type=dict
+        )
 
-        measure_case(n, {
-            "name": "Pure with inner structure",
-            "subs": [
-                {
-                    "type": "whatever",
-                    "numbers": [i, i, i, i]
-                }
-                for i in range(10)
-            ],
-        })
+        @dataclasses.dataclass
+        class Item1:
+            name: str
+            subs: list[dict]
 
-        measure_case(n, {
-            "name": "Containing datetime",
-            "date": datetime.datetime(2023, 7, 16, 10, 25, 0),
-            "counter": 2014123,
-            "number": 132.1
-        })
 
-        measure_case(n, {
-            "name": "Containing dataclass",
-            "box": example_box,
-        })
+        measure_case(
+            n,
+            {
+                "name": "Pure with inner structure",
+                "subs": [
+                    {
+                        "type": "whatever",
+                        "numbers": [i, i, i, i]
+                    }
+                    for i in range(10)
+                ],
+            },
+            as_type=Item1
+        )
+
+        @dataclasses.dataclass
+        class Item2:
+            name: str
+            date: datetime.datetime
+            counter: int
+            number: float
+
+        measure_case(
+            n,
+            {
+                "name": "Containing datetime",
+                "date": datetime.datetime(2023, 7, 16, 10, 25, 0),
+                "counter": 2014123,
+                "number": 132.1
+            },
+            as_type=Item2
+        )
+
+        @dataclasses.dataclass
+        class Item3:
+            name: str
+            box: Box
+
+        measure_case(
+            n,
+            {
+                "name": "Containing dataclass",
+                "box": example_box,
+            },
+            as_type=Item3
+        )
 
 
 if __name__ == '__main__':
