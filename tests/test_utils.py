15cb50585ecfb93690e45fc61e9e9cccc98571cd
Pekka Uronen <pekka@huikea.com>
2023-08-06 11:21:47 +0300

    test dictwithoutkey

---
+++ tests/test_utils.py
@@ -0,0 +1,19 @@
+import decimal
+import pathlib
+import zoneinfo
+
+import pytest
+
+from jsno.utils import DictWithoutKey
+
+
+def test_dict_without_key():
+    base = {"x": 100, "y": 200}
+    it = DictWithoutKey(base=base, key="x")
+
+    assert list(it) == ["y"]
+    assert len(it) == 1
+
+    assert "y" in it
+    assert "x" not in it
+    assert it.get("x") is None
